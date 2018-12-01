7a59af01b7577f5edc159f0c62c2bd8664f5318a
rho2 <rho2bin@gmail.com>
2018-12-01 18:37:47 +0100

    Added the second part of day 1

--- 2018/day1/src/day1.nim
+++ 2018/day1/src/day1.nim
@@ -1,6 +1,7 @@
 import os
 import strutils
 import sequtils
+import sets
 
 when isMainModule:
   let inputs = readFile(paramStr(1)).splitLines.map(parseInt)
@@ -8,4 +9,23 @@ when isMainModule:
   var n = 0
   for i in inputs:
     n += i
+  echo n
+
+  var list = initSet[128]()
+
+  n = 0
+  var c = 0
+  list.incl(0)
+  while true:
+    n += inputs[c]
+    if(list.contains(n)):
+      break
+
+    list.incl(n)
+
+    if(c+1 < len(inputs)):
+      c+=1
+    else:
+      c = 0
+  
   echo n
